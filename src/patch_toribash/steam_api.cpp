0b2abce6b4117832d6882f4af0f4999e45fb85eb
Siarhei Siniak <serega.belarus@gmail.com>
2018-02-04 19:54:47 +0300

    Fine tune steam_api stub
      1. Match function signatures according to the source sdk 2013.
    
      2. Fine tune a SteamUser stub;

--- src/patch_toribash/steam_api.cpp
+++ src/patch_toribash/steam_api.cpp
@@ -1,61 +1,99 @@
 #include "steam_api.h"
 
-int SteamAPI_Shutdown()
+bool SteamAPI_Init()
+{
+	return true;
+}
+
+void * SteamFriends()
 {
 	return 0;
 }
 
-int SteamNetworking()
+bool SteamAPI_RestartAppIfNecessary()
+{
+	return false;
+}
+
+void SteamAPI_Shutdown()
+{
+}
+
+void * SteamNetworking()
 {
 	return 0;
 }
 
-int SteamAPI_RegisterCallback()
+void SteamAPI_RegisterCallback(void * p_callback, int i_callback)
+{
+}
+
+void SteamAPI_UnregisterCallResult(void * p_callback)
+{
+}
+
+void SteamAPI_RegisterCallResult(void * p_callback, SteamAPICall_t h_api_call)
+{
+}
+
+void SteamAPI_UnregisterCallback(void * p_callback, SteamAPICall_t h_api_call)
+{
+}
+
+void * SteamApps()
 {
 	return 0;
 }
 
-int SteamAPI_UnregisterCallResult()
+bool i_steam_user_b_logged_on(void * p_this)
+{
+	return true;
+}
+
+typedef union c_steam_id_t {
+	unsigned long long all_bits;
+} c_steam_id_s;
+
+
+c_steam_id_s i_steam_user_get_steam_id()
+{
+	c_steam_id_s c;
+	c.all_bits = -1;
+
+	return c;
+}
+
+void * SteamUser()
+{
+	typedef struct i_steam_user_t {
+		void * i_steam_user[3];
+	} i_steam_user_s;
+
+	static i_steam_user_s u;
+
+	u.i_steam_user[1] = reinterpret_cast<void *>(&i_steam_user_b_logged_on);
+	u.i_steam_user[2] = reinterpret_cast<void *>(&i_steam_user_get_steam_id);
+
+	static void * u_ptr = reinterpret_cast<void *>(&u);
+
+	return reinterpret_cast<void *>(&u_ptr);
+}
+
+void * SteamMatchmaking()
 {
 	return 0;
 }
 
-int SteamAPI_RegisterCallResult()
+void  SteamAPI_RunCallbacks()
+{
+}
+
+void * SteamUtils()
 {
 	return 0;
 }
 
-int SteamApps()
-{
-	return 0;
-}
-
-int SteamUser()
-{
-	return 0;
-}
-
-int SteamMatchmaking()
-{
-	return 0;
-}
-
-int SteamAPI_RunCallbacks()
-{
-	return 0;
-}
-
-int SteamUtils()
-{
-	return 0;
-}
-
-int SteamAPI_UnregisterCallback()
-{
-	return 0;
-}
-
-int SteamUserStats()
+void * SteamUserStats()
 {
 	return 0;
 }
--- src/patch_toribash/steam_api.h
+++ src/patch_toribash/steam_api.h
@@ -2,18 +2,24 @@
 extern "C" {
 #endif
 
-int SteamAPI_Shutdown();
-int SteamNetworking();
-int SteamAPI_RegisterCallback();
-int SteamAPI_UnregisterCallResult();
-int SteamAPI_RegisterCallResult();
-int SteamApps();
-int SteamUser();
-int SteamMatchmaking();
-int SteamAPI_RunCallbacks();
-int SteamUtils();
-int SteamAPI_UnregisterCallback();
-int SteamUserStats();
+bool SteamAPI_Init();
+void * SteamFriends();
+bool SteamAPI_RestartAppIfNecessary();
+void SteamAPI_Shutdown();
+void * SteamNetworking();
+
+typedef unsigned long long SteamAPICall_t;
+
+void SteamAPI_RegisterCallback(void * p_callback, int i_callback);
+void SteamAPI_UnregisterCallResult(void * p_callback);
+void SteamAPI_RegisterCallResult(void * p_callback, SteamAPICall_t h_api_call);
+void SteamAPI_UnregisterCallback(void * p_callback, SteamAPICall_t h_api_call);
+void * SteamApps();
+void * SteamUser();
+void * SteamMatchmaking();
+void SteamAPI_RunCallbacks();
+void * SteamUtils();
+void * SteamUserStats();
 
 #ifdef __cplusplus
 }
