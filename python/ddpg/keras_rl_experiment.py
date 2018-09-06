248fd47c37d0026ca38b28cad6801fe3037ec465
Siarhei Siniak <serega.belarus@gmail.com>
2018-09-06 16:45:01 +0000

    [~] Refactor actor_critic

--- python/ddpg/keras_rl_experiment.py
+++ python/ddpg/keras_rl_experiment.py
@@ -739,8 +739,18 @@ class RawStatesMemory:
 
             yield b
 
+    def sample_for_ac_train(self, **kwargs):
+        g = self.prioritized_sample_classification(**kwargs)
 
-class Critics:
+        while True:
+            b = next(g)
+
+            i1 = b[0][1][:, -1, :]
+
+            yield ([b[0][0], b[0][1][:, :-2, :], i1.reshape((i1.shape[0], 1) + i1.shape[1:])], b[1])
+
+
+class Models:
     def __init__(
         self,
         batch_size=32,
@@ -750,39 +760,48 @@ class Critics:
 
         self.input_shapes = [ \
             (2 * window_length, 3, 20),
-            (2 * window_length, 22)]
+            (2 * window_length, 22),
+            (2 * (window_length - 1), 22),
+            ]
+
+        self.output_shapes = [
+            (1,),
+            (2,),
+            (22,)
+            ]
 
         self.batch_size = batch_size
         self.window_length = window_length
 
-    def model2(
+    def common_model(
         self,
-    capacity=1,
-    depth=(1, 1),
-    inputs_idxs=numpy.s_[:],
-    activations=('relu',),
-    filters=(16, 16),
-    dropout=0.3,
-    is_regression=False,
-    is_batch_normalized=False,
-    use_lstm=False):
+        capacity=1,
+        depth=[1, 1],
+        inputs_idxs=numpy.s_[:],
+        activations=('relu',),
+        filters=(16, 16),
+        dropout=0.3,
+        is_regression=False,
+        is_critic=True,
+        is_batch_normalized=False,
+        use_lstm=False):
 
         if type(depth) is not list:
             depth = [depth, depth]
 
         assert len(depth) == 2
 
-        inputs = [keras.layers.Input(
+        all_inputs = [keras.layers.Input(
             batch_shape=(self.batch_size,) + input_shape) for input_shape in self.input_shapes]
+
+        inputs = []
 
         net = []
 
-        for index, input_layer in zip(inputs_idxs, numpy.array(inputs)[inputs_idxs]):
+        for index, input_layer in zip(inputs_idxs, numpy.array(all_inputs)[inputs_idxs]):
             x = input_layer
 
             if index in [0]:
-                #x = keras.layers.Reshape(
-                #    target_shape=tuple([1,] + x.shape.as_list()[1:]))(x)
                 x = keras.layers.Conv2D(
                     data_format='channels_first',
                     filters=filters[0],
@@ -791,10 +810,7 @@ class Critics:
                     activation=activations[0],
                     kernel_initializer='glorot_normal',
                     bias_initializer='glorot_normal',
-                    #dropout=0.75,
-                    #recurrent_dropout=0.8,
                     strides=(1, 1),
-                    #return_sequences=True
                     )(x)
                 x = keras.layers.Dropout(dropout)(x)
                 if is_batch_normalized:
@@ -807,10 +823,7 @@ class Critics:
                     activation=activations[0],
                     kernel_initializer='glorot_normal',
                     bias_initializer='glorot_normal',
-                    #dropout=0.75,
-                    #recurrent_dropout=0.8,
                     strides=(1, 1),
-                    #return_sequences=True
                     )(x)
                 x = keras.layers.Dropout(dropout)(x)
                 if is_batch_normalized:
@@ -824,10 +837,7 @@ class Critics:
                         activation=activations[0],
                         kernel_initializer='glorot_normal',
                         bias_initializer='glorot_normal',
-                        #dropout=0.75,
-                        #recurrent_dropout=0.8,
                         strides=(3, 4),
-                        #return_sequences=True
                         )(x)
                     x = keras.layers.Dropout(dropout)(x)
                     if is_batch_normalized:
@@ -841,10 +851,7 @@ class Critics:
                         activation=activations[0],
                         kernel_initializer='glorot_normal',
                         bias_initializer='glorot_normal',
-                        #dropout=0.75,
-                        #recurrent_dropout=0.75,
                         strides=(1, 4),
-                        #return_sequences=False
                         )(x)
                     x = keras.layers.Dropout(dropout)(x)
                     if is_batch_normalized:
@@ -857,17 +864,11 @@ class Critics:
                         kernel_initializer='glorot_normal',
                         recurrent_initializer='glorot_normal',
                         bias_initializer='glorot_normal',
-                        #data_format='channels_first',
-                        #filters=1,
-                        #kernel_size=(1, 1),
-                        #padding='same',
                         activation=activations[0],
-                        dropout=0.3,
-                        recurrent_dropout=0.3,
-                        #strides=(1, 4),
+                        dropout=dropout,
+                        recurrent_dropout=dropout,
                         return_sequences=False)(x)
-                #x = keras.layers.BatchNormalization()(x)
-            elif index in [1]:
+            elif index in [1, 2]:
                 x = keras.layers.Conv1D(
                     data_format='channels_first',
                     filters=filters[0],
@@ -876,10 +877,7 @@ class Critics:
                     activation=activations[0],
                     kernel_initializer='glorot_normal',
                     bias_initializer='glorot_normal',
-                    #dropout=0.75,
-                    #recurrent_dropout=0.8,
                     strides=(1,),
-                    #return_sequences=True
                     )(x)
                 x = keras.layers.Dropout(dropout)(x)
                 if is_batch_normalized:
@@ -892,10 +890,7 @@ class Critics:
                     activation=activations[0],
                     kernel_initializer='glorot_normal',
                     bias_initializer='glorot_normal',
-                    #dropout=0.75,
-                    #recurrent_dropout=0.8,
                     strides=(1,),
-                    #return_sequences=True
                     )(x)
                 x = keras.layers.Dropout(dropout)(x)
                 if is_batch_normalized:
@@ -908,10 +903,7 @@ class Critics:
                     activation=activations[0],
                     kernel_initializer='glorot_normal',
                     bias_initializer='glorot_normal',
-                    #dropout=0.75,
-                    #recurrent_dropout=0.8,
                     strides=(3,),
-                    #return_sequences=True
                     )(x)
                 x = keras.layers.Dropout(dropout)(x)
                 if is_batch_normalized:
@@ -924,10 +916,7 @@ class Critics:
                     activation=activations[0],
                     kernel_initializer='glorot_normal',
                     bias_initializer='glorot_normal',
-                    #dropout=0.75,
-                    #recurrent_dropout=0.8,
                     strides=(3,),
-                    #return_sequences=True
                     )(x)
                 x = keras.layers.Dropout(dropout)(x)
                 if is_batch_normalized:
@@ -940,10 +929,7 @@ class Critics:
                     activation=activations[0],
                     kernel_initializer='glorot_normal',
                     bias_initializer='glorot_normal',
-                    #dropout=0.75,
-                    #recurrent_dropout=0.8,
                     strides=(2,),
-                    #return_sequences=True
                     )(x)
                 x = keras.layers.Dropout(dropout)(x)
                 if is_batch_normalized:
@@ -956,57 +942,40 @@ class Critics:
                         kernel_initializer='glorot_normal',
                         recurrent_initializer='glorot_normal',
                         bias_initializer='glorot_normal',
-                        #data_format='channels_first',
-                        #filters=1,
-                        #kernel_size=(1, 1),
-                        #padding='same',
                         activation=activations[0],
-                        dropout=0.3,
-                        recurrent_dropout=0.3,
-                        #strides=(1, 4),
+                        dropout=dropout,
+                        recurrent_dropout=dropout,
                         return_sequences=False)(x)
 
             if len(x.shape) > 2:
                 x = keras.layers.Flatten()(x)
 
-            #x = keras.layers.Conv1D(
-
             for d in range(depth[0]):
                 x = keras.layers.Dense(32 << capacity, activation=activations[0])(x)
-                #x = keras.layers.Dropout(0.1)(x)
+                x = keras.layers.Dropout(0.1)(x)
 
             net.append(x)
+
+            inputs.append(input_layer)
 
         if len(net) > 1:
             x = keras.layers.Concatenate()(net)
         else:
             x = net[0]
 
-        #x2 = keras.layers.Conv2D(
-        #    64,
-        #    (3, 3),
-        #    data_format='channels_first',
-        #    activation='selu')(mutual_dist_feature_input)
-        #x2 = keras.layers.MaxPooling2D(pool_size=(3, 1), data_format='channels_first')(x2)
-        #x2 = keras.layers.Conv2D(64, (16, 1), data_format='channels_first', activation='selu')(x2)
-        #x2 = keras.layers.MaxPooling2D(pool_size=(16, 1), data_format='channels_first')(x2)
-        #x2 = keras.layers.Conv2D(64, (16, 1), data_format='channels_first', activation='selu')(x2)
-        #x2 = keras.layers.MaxPooling2D(pool_size=(16, 1), data_format='channels_first')(x2)
-
-        #x2 = keras.layers.Conv2D(2, (4, 3), data_format='channels_first',
-        #        activation='selu')(mutual_dist_feature_input)
-        #x2 = keras.layers.MaxPooling2D(pool_size=(32, 1), data_format='channels_first')(x2)
-
         y = x
 
         for d in range(depth[1]):
             y = keras.layers.Dense(32 << capacity, activation=activations[0])(y)
-            #y = keras.layers.Dropout(0.2)(y)
+            y = keras.layers.Dropout(dropout)(y)
 
-        if is_regression:
-            y = keras.layers.Dense(1, activation='elu')(y)
+        if is_critic:
+            if is_regression:
+                y = keras.layers.Dense(*self.output_shapes[0], activation='elu')(y)
+            else:
+                y = keras.layers.Dense(*self.output_shapes[1], activation='softmax')(y)
         else:
-            y = keras.layers.Dense(2, activation='softmax')(y)
+            y = keras.layers.Dense(*self.output_shapes[2], activation='elu')(y)
 
         model = keras.models.Model(inputs=inputs, outputs=y)
 
@@ -1014,100 +983,25 @@ class Critics:
 
         return model
 
-    def model3(self):
-        state_input = keras.layers.Input(
-            batch_shape=(self.batch_size, 2 * self.window_length) + self.state_shape,
-            name='state_input')
+    def actor_critic(self, actor, critic):
+        assert not critic.trainable
 
-        mutual_dist_feature_input = keras.layers.Input(
-            batch_shape=(self.batch_size, self.window_length,) + self.mutual_dist_feature_input,
-            name='mutual_dist_feature_input')
+        i1_6 = keras.layers.Input(batch_shape=(self.batch_size, 1, self.input_shapes[1][-1]))
 
-        x = keras.layers.Flatten()(state_input)
-        x = keras.layers.Dense(256, activation='selu')(x)
-        x = keras.layers.Dropout(0.3)(x)
+        i1_5_reshape = keras.layers.Reshape(
+            target_shape=(i1_6.shape.as_list()[1:]))(actor.outputs[0])
 
-        #x2 = keras.layers.Conv2D(
-        #    64,
-        #    (3, 3),
-        #    data_format='channels_first',
-        #    activation='selu')(mutual_dist_feature_input)
-        #x2 = keras.layers.MaxPooling2D(pool_size=(3, 1), data_format='channels_first')(x2)
-        #x2 = keras.layers.Conv2D(64, (16, 1), data_format='channels_first', activation='selu')(x2)
-        #x2 = keras.layers.MaxPooling2D(pool_size=(16, 1), data_format='channels_first')(x2)
-        #x2 = keras.layers.Conv2D(64, (16, 1), data_format='channels_first', activation='selu')(x2)
-        #x2 = keras.layers.MaxPooling2D(pool_size=(16, 1), data_format='channels_first')(x2)
-        #x2 = keras.layers.Dropout(0.1)(x2)
+        i1_all = keras.layers.Concatenate(axis=1)([actor.inputs[1], i1_5_reshape, i1_6])
 
-        #x2 = keras.layers.Conv2D(2, (4, 3), data_format='channels_first',
-        #        activation='selu')(mutual_dist_feature_input)
-        #x2 = keras.layers.MaxPooling2D(pool_size=(32, 1), data_format='channels_first')(x2)
-        x2 = keras.layers.Flatten()(mutual_dist_feature_input)
-        x2 = keras.layers.Dense(256, activation='selu')(x2)
-        x2 = keras.layers.Dropout(0.3)(x2)
+        return keras.models.Model(
+            inputs=[actor.inputs[0], actor.inputs[1], i1_6],
+            outputs=critic([actor.inputs[0], i1_all]))
 
-        x3 = keras.layers.Concatenate()([x, x2])
-        y = keras.layers.Dense(256, activation='selu')(x3)
-        y = keras.layers.Dropout(0.5)(y)
-        y = keras.layers.Dense(1, activation='linear')(y)
-
-        model = keras.models.Model(inputs=[state_input, mutual_dist_feature_input], outputs=y)
-
-        model.summary()
-
-        return model
-
-    def slice_batches_across_devices(self, model, worker_devices, master_cpu):
-        def get_slice(data, i, parts):
-            shape = tensorflow.shape(data)
-            batch_size = shape[:1]
-            input_shape = shape[1:]
-            step = batch_size // parts
-            if i == len(worker_devices) - 1:
-                size = batch_size - step * i
-            else:
-                size = step
-            size = tensorflow.concat([size, input_shape], axis=0)
-            stride = tensorflow.concat([step, input_shape * 0], axis=0)
-            start = stride * i
-            return tensorflow.slice(data, start, size)
-
-        all_outputs = []
-        for i in range(len(model.outputs)):
-            all_outputs.append([])
-
-        # Place a copy of the model on each GPU,
-        # each getting a slice of the inputs.
-        for i, dev in enumerate(worker_devices):
-            with tensorflow.device(dev.name):
-                with tensorflow.name_scope('replica_%d' % i):
-                    inputs = []
-                    # Retrieve a slice of the input.
-                    for x in model.inputs:
-                        input_shape = tuple(x.get_shape().as_list())[1:]
-                        slice_i = keras.layers.Lambda(get_slice,
-                                         output_shape=input_shape,
-                                         arguments={'i': i,
-                                                    'parts': len(worker_devices)})(x)
-                        inputs.append(slice_i)
-
-                    # Apply model on slice
-                    # (creating a model replica on the target device).
-                    outputs = model(inputs)
-                    if not isinstance(outputs, list):
-                        outputs = [outputs]
-
-                    # Save the outputs for merging back together later.
-                    for o in range(len(outputs)):
-                        all_outputs[o].append(outputs[o])
-
-        # Merge outputs on CPU.
-        with tensorflow.device(master_cpu.name):
-            merged = []
-            for name, outputs in zip(model.output_names, all_outputs):
-                merged.append(keras.layers.concatenate(outputs,
-                                          axis=0, name=name))
-            return keras.models.Model(model.inputs, merged)
+    def ac_compile(self, ac):
+        ac.compile(
+            optimizer=keras.optimizers.Adam(lr=1e-4),
+            loss=lambda yt, yp: \
+                -keras.backend.relu((yp[:, 1] - 0.5) * 2))
 
 
 class Helpers:
@@ -1148,84 +1042,22 @@ class Helpers:
             epochs=epochs,
             verbose=1)
 
-    def plot_samples(self, sample_size=100):
-        s = self.rsm.prioritized_sample_classification(batch_size=sample_size)
+    def ac_train(
+        self,
+        model,
+        train_steps_multiplier=1.0,
+        test_steps_multiplier=1.0,
+        epochs=10):
 
-        f, ax = matplotlib.pyplot.subplots(4, 1, sharex=True)
-
-        ax = [[o] for o in ax]
-
-        im0 = ax[0][0].pcolormesh(s[0][5][:, 0, :].T)
-        im1 = ax[1][0].pcolormesh(s[0][3][:, 0, :].T)
-        ax[2][0].plot(numpy.argmax(s[1], axis=1))
-
-        dc = numpy.clip(s[0][5][:, 0, :], 0, 2.0)
-        im3 = ax[3][0].pcolormesh(dc.T)
-
-        f.colorbar(im0, ax=ax[0][0])
-        f.colorbar(im1, ax=ax[1][0])
-        f.colorbar(im1, ax=ax[2][0])
-        f.colorbar(im3, ax=ax[3][0])
-
-        f.show()
-
-    def train_classification_sklearn(self, dataset_size=10000, batch_size=128):
-        test_size = int(dataset_size * 0.3)
-        train_size = int(dataset_size * 0.7)
-
-        def to_sklearn(b):
-            x = numpy.hstack([b[0][5][:, -1:, :], b[0][3]])
-            y = b[1]
-
-            return x.reshape(x.shape[0], -1), \
-                numpy.argmax(y, axis=1)
-
-        for ep in range(1):
-            max_iter = 2000
-
-            for cf in [ \
-                #sklearn.svm.SVC(max_iter=max_iter),
-                sklearn.neural_network.MLPClassifier(),
-                #sklearn.ensemble.RandomForestClassifier(
-                #    20,
-                #    max_depth=20,
-                #    random_state=0,
-                #    n_jobs=4,
-                #    max_features='log2',
-                #    warm_start=True)
-                #sklearn.ensemble.AdaBoostClassifier(
-                #    sklearn.neural_network.MLPClassifier(),
-                #    n_estimators=600,
-                #    learning_rate=1)
-                #sklearn.cluster.MiniBatchKMeans()
-                ]:
-                for bs in range(train_size // batch_size):
-                    print("\rtrain: batch = %d of %d" % (bs, train_size // batch_size), end='')
-                    if bs > 0:
-                        cf.partial_fit(*to_sklearn(self.rsm.prioritized_sample_classification(
-                            batch_size=batch_size, is_test=False)))
-                    else:
-                        cf.partial_fit(*to_sklearn(self.rsm.prioritized_sample_classification(
-                            batch_size=batch_size, is_test=False)), classes=[0, 1])
-
-                y_test = []
-                y_pred = []
-
-                for bs in range(test_size // batch_size):
-                    print("\rvalidate: batch = %d of %d" % (bs, test_size // batch_size), end='')
-                    batch = to_sklearn(self.rsm.prioritized_sample_classification(
-                        batch_size=batch_size, is_test=False))
-                    y_test.append(batch[1])
-                    y_pred.append(cf.predict(batch[0]))
-
-                y_test = numpy.concatenate(y_test)
-                y_pred = numpy.concatenate(y_pred)
-
-                test_mae = numpy.sum(numpy.abs(y_pred - y_test))
-                test_accuracy = numpy.sum(y_pred == y_test) / y_test.shape[0]
-
-                print('cf = %s, ep = %d, bs = %d, max_iter = %d, mae = %lf, accuracy = %lf' % \
-                    (str(cf.__class__), ep, -1, max_iter, test_mae, test_accuracy))
+        model.fit_generator(
+            self.rsm.sample_for_ac_train(),
+            validation_data=self.rsm.sample_for_ac_train(is_test=True),
+            steps_per_epoch=int(self.rsm.nb_entries() // \
+                self.rsm.attrs()['batch_size'] * train_steps_multiplier),
+            validation_steps=int(self.rsm.nb_entries(is_test=True) // \
+                self.rsm.attrs()['batch_size'] * test_steps_multiplier),
+            epochs=epochs,
+            verbose=1)
 
     def mine_dataset(self, client_key, steps_limit, save_prefix):
         iteration = 0
